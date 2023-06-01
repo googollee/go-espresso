b291874da6fe31bb1d8823faf89e7ff73148f997
Googol Lee <i@googol.im>
2023-06-01 10:07:06 +0200

    Draft with registering context.

---
+++ process_context.go
@@ -0,0 +1,75 @@
+package espresso
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type processContext[Data any] struct {
+	context.Context
+	data           *Data
+	request        *http.Request
+	responseWriter http.ResponseWriter
+	pathParams     httprouter.Params
+	handlers       []Handler[Data]
+
+	isAborted bool
+	err       error
+}
+
+func newProcessContext[Data any](ctx context.Context, req *http.Request, writer http.ResponseWriter, params httprouter.Params, initData Data, handlers []Handler[Data]) Context[Data] {
+	return &processContext[Data]{
+		Context:        ctx,
+		data:           &initData,
+		request:        req,
+		responseWriter: writer,
+		pathParams:     params,
+		handlers:       handlers,
+	}
+}
+
+func (c *processContext[Data]) BindURLParam(name string, v any) error {
+	return nil
+}
+
+func (c *processContext[Data]) BindQueryParam(name string, v any) error {
+	return nil
+}
+
+func (c *processContext[Data]) BindHead(name string, v any) error {
+	return nil
+}
+
+func (c *processContext[Data]) Endpoint(method, path string, middlewares ...Handler[Data]) {
+}
+
+func (c *processContext[Data]) Data() *Data {
+	return c.data
+}
+
+func (c *processContext[Data]) Abort(err error) {
+	c.isAborted = true
+	c.err = err
+}
+
+func (c *processContext[Data]) Error() error {
+	return c.err
+}
+
+func (c *processContext[Data]) Next() {
+	for len(c.handlers) > 0 && !c.isAborted {
+		handler := c.handlers[0]
+		c.handlers = c.handlers[1:]
+		handler(c)
+	}
+}
+
+func (c *processContext[Data]) Request() *http.Request {
+	return c.request
+}
+
+func (c *processContext[Data]) ResponseWriter() http.ResponseWriter {
+	return c.responseWriter
+}
--- server.go
+++ server.go
@@ -41,14 +41,14 @@ func (s *Server[ContextData]) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 
 func (s *Server[ContextData]) GET(path string, funcs ...Handler[ContextData]) {
 	s.router.GET(path, func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-		ctx := newContext(r.Context(), r, w, params, s.initCtxData, funcs)
+		ctx := newProcessContext(r.Context(), r, w, params, s.initCtxData, funcs)
 		ctx.Next()
 	})
 }
 
 func (s *Server[ContextData]) POST(path string, funcs ...Handler[ContextData]) {
 	s.router.POST(path, func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-		ctx := newContext(r.Context(), r, w, params, s.initCtxData, funcs)
+		ctx := newProcessContext(r.Context(), r, w, params, s.initCtxData, funcs)
 		ctx.Next()
 	})
 }
